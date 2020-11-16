908f04afb8d7fd06890ab2bf86f460ab0aeb0ba7
Gauthier Nourdin <gauthier.nourdin@protonmail.com>
2020-11-16 11:45:49 +0100

    Mise en place architecture site, ajout JSTL 1.2 dans les JSP.

---
+++ projet-poei-site-e-commerce/src/org/eclipse/controller/VendeurProduitsServlet.java
@@ -0,0 +1,22 @@
+package org.eclipse.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/vendeur/produits")
+public class VendeurProduitsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

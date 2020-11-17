476c0589e106490ead839f37ecc5d8a77856701d
Illithian29 <Illithian29@192.168.1.11>
2020-11-17 05:44:33 +0100

    Transfert vue panier dans son dossier indépendant

--- projet-poei-site-e-commerce/src/org/eclipse/filter/PanierFilter.java
+++ projet-poei-site-e-commerce/src/org/eclipse/filter/PanierFilter.java
@@ -12,30 +12,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.eclipse.model.Client;
 import org.eclipse.model.Vendeur;
 
-@WebFilter("/*")
-public class PrivateFilter implements Filter {
+@WebFilter("/panier/panier")
+public class PanierFilter implements Filter {
 
 	public void destroy() {
 	}
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		HttpSession session = req.getSession();
-		Client clientConnected = (Client) session.getAttribute("client");
 		Vendeur vendeurConnected = (Vendeur) session.getAttribute("vendeur");
-		String chemin = req.getServletPath();
-		if (clientConnected != null || vendeurConnected != null || chemin.equals("/") || chemin.equals("/index")
-				|| chemin.equals("/produit/produit") || chemin.equals("/client/connexion")
-				|| chemin.equals("/vendeur/connexion") || chemin.equals("/client/inscription")
-				|| chemin.equals("/vendeur/inscription") || chemin.equals("/panier/panier"))
+		if (vendeurConnected == null)
 			chain.doFilter(request, response);
 		else
-			res.sendRedirect(req.getContextPath());
+			res.sendRedirect(req.getContextPath() + "/vendeur/produits");
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
